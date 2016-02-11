1afda605600d0da3a557144a21b44c1aac582497
Kevin Chesser <kchesser@Kevins-MacBook-Pro-2.local>
2016-02-11 14:48:33 -0500

    Things can now be dragged

--- dropzones.js
+++ dropzones.js
@@ -1,6 +1,6 @@
 /* The dragging code for '.draggable' from the demo above
  *  * applies to this demo as well so it doesn't have to be repeated. */
-
+window.onload = function() {
 // enable draggables to be dropped into this
  interact('.dropzone').dropzone({
 //   // only accept elements matching this CSS selector
@@ -38,3 +38,51 @@
                                                                                                  event.target.classList.remove('drop-target');
                                                                                                    }
                                                                                                    });
+
+// target elements with the "draggable" class
+ interact('.draggable')
+   .draggable({
+//       // enable inertial throwing
+           inertia: true,
+//               // keep the element within the area of it's parent
+                   restrict: {
+                         restriction: "parent",
+                               endOnly: true,
+                                     elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+                                         },
+                                             // enable autoScroll
+                                                 autoScroll: true,
+
+//                                                     // call this function on every dragmove event
+                                                         onmove: dragMoveListener,
+//                                                             // call this function on every dragend event
+                                                                 onend: function (event) {
+  									 var textEl = event.target.querySelector('p');
+
+                                                                             textEl && (textEl.textContent =
+//                                                                                     'moved a distance of '
+                                                                                             + (Math.sqrt(event.dx * event.dx +
+                                                                                                                  event.dy * event.dy)|0) + 'px');
+                                                                                                                      }
+                                                                                                                        });
+
+                                                                                                                          function dragMoveListener (event) {
+                                                                                                                              var target = event.target,
+//                                                                                                                                      // keep the dragged position in the data-x/data-y attributes
+                                                                                                                                              x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+                                                                                                                                                      y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+//                                                                                                                                                          // translate the element
+                                                                                                                                                              target.style.webkitTransform =
+                                                                                                                                                                  target.style.transform =
+                                                                                                                                                                        'translate(' + x + 'px, ' + y + 'px)';
+
+//                                                                                                                                                                            // update the posiion attributes
+                                                                                                                                                                                target.setAttribute('data-x', x);
+                                                                                                                                                                                    target.setAttribute('data-y', y);
+                                                                                                                                                                                      }
+//
+//                                                                                                                                                                                        // this is used later in the resizing and gesture demos
+                                                                                                                                                                                          window.dragMoveListener = dragMoveListener;
+
+}
