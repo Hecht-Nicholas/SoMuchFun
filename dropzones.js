ab996321bca30e8e8a53b7110ef0e74e51675782
Kevin Chesser <kchesser@Kevins-MBP-2.hsd1.fl.comcast.net>
2016-02-12 12:13:22 -0500

    Now snapps to the outside dropzone

--- dropzones.js
+++ dropzones.js
@@ -18,13 +18,13 @@ window.onload = function() {
         // feedback the possibility of a drop
             dropzoneElement.classList.add('drop-target');
             draggableElement.classList.add('can-drop');
-            draggableElement.textContent = 'Dragged in';
+            draggableElement.textContent = 'Active Camera';
      },
      ondragleave: function (event) {
      // remove the drop feedback style
         event.target.classList.remove('drop-target');
         event.relatedTarget.classList.remove('can-drop')   
-	event.relatedTarget.textContent = 'Dragged out';
+	event.relatedTarget.textContent = 'Camera Off';
      },
      ondrop: function (event) {
        event.relatedTarget.textContent = 'Dropped';
@@ -36,6 +36,8 @@ window.onload = function() {
      }
 });
 
+
+/*
 // target elements with the "draggable" class
  interact('.draggable')
    .draggable({
@@ -75,4 +77,53 @@ window.onload = function() {
          }
 	 // this is used later in the resizing and gesture demos
 	 window.dragMoveListener = dragMoveListener;
+*/
+	 
+	interact('.draggable')
+   .draggable({
+      // enable inertial throwing
+      inertia: true,
+      // keep the element within the area of it's parent
+      restrict: {
+        restriction: '.non-activedropzone',
+        endOnly: true,
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+      },
+      // enable autoScroll
+      autoScroll: true,
+      // call this function on every dragmove event
+      onmove: dragMoveListener,
+      // call this function on every dragend event
+      //onend: function (event) {
+      //var textEl = event.target.querySelector('p');
+      //textEl && (textEl.textContent = 'moved a distance of '
+      //+ (Math.sqrt(event.dx * event.dx +
+      //event.dy * event.dy)|0) + 'px');
+      //}
+    });
+    function dragMoveListener (event) {
+      var target = event.target,
+          // keep the dragged position in the data-x/data-y attributes
+          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+          // translate the element
+          target.style.webkitTransform =
+          target.style.transform =
+          'translate(' + x + 'px, ' + y + 'px)'
+	  // update the posiion attributes
+	  target.setAttribute('data-x', x);
+	  target.setAttribute('data-y', y);
+         }
+	 // this is used later in the resizing and gesture demos
+	 window.dragMoveListener = dragMoveListener;
+ 
+	 
+	 
+	 
+	 
+
+	 
+	 
+	 
 }
